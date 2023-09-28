7733a0a8ec271a5e254f724985a5218a4a3b40d4
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2023-09-27 17:18:21 -0700

    Player Camera Rotation
    Added camera movement to the player. Additionally the player will move according to the direction the camera is facing.

--- GhostRunnersProject/Assets/Scripts/PlayerController.cs
+++ GhostRunnersProject/Assets/Scripts/PlayerController.cs
@@ -1,36 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Callbacks;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering;
 //Garcia, Mario
 //9/23/23
-//Moves the player and applies gravity. 
+//Moves the player/camer and applies gravity. 
 public class PlayerController : MonoBehaviour
 {   
     private Vector3 movementInput;
     private Vector3 velocity;
     [SerializeField] private CharacterController controller;
-    [SerializeField]private float speed;
-    [SerializeField]private float gravity = -9.81f;
+    [SerializeField] private Transform cam;
+    [SerializeField] private float speed;
+    [SerializeField] private float gravity = -9.81f;
+
+    void Start()
+    {
+        //Hiding the cursor on start (press escape to get back cursor)
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     void Update()
     {   
-        //getting the input from the player and calling the move function
-        movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         Move();
     }
 
     private void Move()
     {
-        //getting the direction of the player inputs 
-        Vector3 moveVec = transform.TransformDirection(movementInput);
+        //getting the input from the player and calling the move function
+        movementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
         //checking if the player is on the ground
         if(controller.isGrounded)
         {   
-            //makes it so that when you fall off you don't instantly fall at max gracvity. 
+            //makes it so that when you fall off you don't instantly fall at max gravity. 
             velocity.y = -1f;
         }
         else
@@ -39,9 +46,22 @@ public class PlayerController : MonoBehaviour
             velocity.y -= gravity * -2f * Time.deltaTime;
         }
 
-        //moving the player 
-        controller.Move(moveVec * speed * Time.deltaTime);
-        controller.Move(velocity * Time.deltaTime);
-        
+        //Checks to see if the player is moving
+        if(movementInput.magnitude >= 0.1f)
+        {
+            //getting the direction of the player inputs 
+            Vector3 moveVec = transform.TransformDirection(movementInput);
+
+            
+
+            //move the player according to the cameras direction
+            float targetAngle = Mathf.Atan2(movementInput.x, movementInput.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            //moving the player
+            Vector3 direction = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(direction * speed * Time.deltaTime);
+            controller.Move(velocity * Time.deltaTime);
+        }
     }
 }
