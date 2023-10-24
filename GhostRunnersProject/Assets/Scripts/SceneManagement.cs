eb1ad4c7d696b5335c41060756b3012ff1daf885
elmerdeleon98 <elmerdeleon98@gmail.com>
2023-10-24 10:48:14 -0700

    Restart mechanic
    Added game over screen as well as buttons, will add coding next.

--- GhostRunnersProject/Assets/Scripts/SceneManagement.cs
+++ GhostRunnersProject/Assets/Scripts/SceneManagement.cs
@@ -5,17 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-   
+   //private void Restart()
+    //{
+       // SceneSwitch.instance.switchScene(2);
+    //}
 }
