ea9117b0ce2955462c76868525dc2524f8845dc7
MarioGarcia13 <77987626+MarioGarcia13@users.noreply.github.com>
2023-11-08 13:24:56 -0800

    Added Daylight Timer and Random Drops
    DaylightTimer is made into a prefab and has a timer that you can set manually. The random drops are the healthpack and the battery

--- GhostRunnersProject/Assets/Scripts/Flashlight.cs
+++ GhostRunnersProject/Assets/Scripts/Flashlight.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEditor;
 using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
+    
+    public Transform spawnPos;
+    public GameObject healthPack;
+    public GameObject battery;
+
+    public float dropProbability = 0.2f;
+
     private void OnTriggerEnter(Collider other)
     {
         //checks if the flashlight detects the enemy
@@ -12,7 +21,34 @@ public class Flashlight : MonoBehaviour
             //deactivates the enemy if detected.
             other.gameObject.SetActive(false);
             Player.batteryJuice--;
+            RandomDrop();
             Debug.Log("enemy was attacked");
+
+        }
+    }
+
+    private void RandomDrop()
+    {
+        float randomValue = Random.Range(0f, 1f);
+        int randomDrop = Random.Range(0, 2);
+        bool drop = randomValue <= dropProbability;
+
+        if(drop)
+        {
+            switch (randomDrop) 
+            {  
+                case 0:
+                    //instantiate drop
+                    Instantiate(healthPack, spawnPos.position, spawnPos.rotation);
+                    break;
+                case 1:
+                    //instantiate drop
+                    Instantiate(battery, spawnPos.position, spawnPos.rotation);
+                    break;
+                default:
+                    break;               
+            }
+            Debug.Log("Item Dropped");
 
         }
     }
