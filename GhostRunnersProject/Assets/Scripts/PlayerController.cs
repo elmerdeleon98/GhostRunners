bcc941ed6aa3be3cba954fb23580a41fd6a2190a
elmerdeleon98 <elmerdeleon98@gmail.com>
2023-11-14 02:44:05 -0800

    build issues fix

--- GhostRunnersProject/Assets/Scripts/PlayerController.cs
+++ GhostRunnersProject/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Build;
 using UnityEngine;
 using UnityEngine.Rendering;
 //Garcia, Mario
