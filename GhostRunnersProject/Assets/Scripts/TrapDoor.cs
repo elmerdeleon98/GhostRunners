a966e6fc27a1f1cd65687837871ad6629adafc95
Rex <mitchellmoreno7@gmail.com>
2023-11-15 23:53:03 -0800

    Door Traps Added Lvl 3
    Door traps are now functional, only blocking player when turned red

--- GhostRunnersProject/Assets/Scripts/TrapDoor.cs
+++ GhostRunnersProject/Assets/Scripts/TrapDoor.cs
@@ -4,63 +4,37 @@ using UnityEngine;
 
 public class TrapDoor : MonoBehaviour
 {
-    public Renderer doorRenderer; // Reference to the Renderer component of the door
-    public Collider doorCollider; // Reference to the Collider component of the door
-    public Color blockingColor = Color.red; // Color when blocking players
-    public Color passThroughColor = Color.green; // Color when allowing players to pass through
-    public float blockDuration = 2.0f; // Duration the door is blocking players
-    public float openDuration = 1.0f; // Duration the door is open for players to pass through
+    public Renderer doorRenderer;
+    public Collider doorCollider;
+    public Color redColor = Color.red; //red color door
+    public float disappearDuration = 2f; //timer for door
 
     private void Start()
     {
-        StartCoroutine(ActivateColorDoorPeriodically());
+        StartCoroutine(ChangeColorAndDisappear());
     }
 
-    private IEnumerator ActivateColorDoorPeriodically()
+    private IEnumerator ChangeColorAndDisappear()
     {
         while (true)
         {
-            //block players (change color and become transparent)
-            SetDoorColor(blockingColor);
-            SetDoorAlpha(0.5f); 
-            doorCollider.enabled = true;
-            yield return new WaitForSeconds(blockDuration);
+            //changes door color to red
+            SetDoorColor(redColor);
 
-            //allow players to pass through
-            SetDoorColor(passThroughColor);
-            SetDoorAlpha(1f);
+            yield return new WaitForSeconds(disappearDuration);
+
+            doorRenderer.enabled = false;
             doorCollider.enabled = false;
-            yield return new WaitForSeconds(openDuration);
+
+            yield return new WaitForSeconds(disappearDuration);
+
+            doorRenderer.enabled = true;
+            doorCollider.enabled = true;
         }
     }
 
     private void SetDoorColor(Color color)
     {
         doorRenderer.material.color = color;
-    }
-
-    private void SetDoorAlpha(float alpha)
-    {
-        Color currentColor = doorRenderer.material.color;
-        currentColor.a = alpha;
-        doorRenderer.material.color = currentColor;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") && doorRenderer.material.color == passThroughColor)
-        {
-            // Allow the player to pass through the door by disabling the collider
-            doorCollider.enabled = false;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            // Re-enable the collider when the player exits the trigger
-            doorCollider.enabled = true;
-        }
     }
 }
