30e98aeeb0e340ee408d5a8b5ed8d2117fce3a35
elmerdeleon98 <elmerdeleon98@gmail.com>
2023-11-28 10:16:24 -0800

    timer
    timer

---
+++ GhostRunnersProject/Assets/Timer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    //light variables
+    public GameObject moveLight;
+    public Transform targetPosition;
+    public float moveSpeed = 1f;
+    private Vector3 endPosition;
+    private bool isMoving = false;
+
+    //timer variables
+    public float timer = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Vector3 moveLight;
+
+        timer -= Time.deltaTime;
+
+        Debug.Log(timer);
+
+        if (timer < 0)
+        {
+            MoveLight();
+        }
+
+    }
+
+    public void MoveLight()
+    {
+        endPosition = moveLight.transform.position + Vector3.up * 2.0f;
+        isMoving = true;
+
+        if(isMoving) 
+        {
+            moveLight.transform.position = Vector3.Lerp(moveLight.transform.position, endPosition, moveSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(moveLight.transform.position, endPosition) < 0.01f)
+            {
+                isMoving = false;
+            }
+        }
+    }
+
+}
