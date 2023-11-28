e8520d29f4a831d4b66ef03106f8aa68d976a273
elmerdeleon98 <elmerdeleon98@gmail.com>
2023-11-28 11:11:16 -0800

    Coins Ui
    Added Ui for coins

--- GhostRunnersProject/Assets/Scripts/UiManager.cs
+++ GhostRunnersProject/Assets/Scripts/UiManager.cs
@@ -9,6 +9,7 @@ public class UiManager : MonoBehaviour
     public TMP_Text playerHealthText;
     public TMP_Text playerBatteryText;
     public TMP_Text playerLivesText;
+    public TMP_Text playerCoinsText;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +17,7 @@ public class UiManager : MonoBehaviour
         playerHealthText.text = "Player Health:" + Player.playerHealth;
         playerBatteryText.text = "Charge Count:" + Player.batteryJuice;
         playerLivesText.text = "Lives:" + PlayerController.lives;
+        playerCoinsText.text = "Coins:" + DoorUnlock.coinCount;
     }
  
 }
