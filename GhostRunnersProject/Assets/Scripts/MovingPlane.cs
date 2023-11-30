7f99683e05edb422c0d867fef73f4e9878b0dfa5
Rex <mitchellmoreno7@gmail.com>
2023-11-29 20:53:35 -0800

    Moving Planes added
    moving planes script added onto planes for level 3

---
+++ GhostRunnersProject/Assets/Scripts/MovingPlane.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlane : MonoBehaviour
+{
+    public Transform startPos;
+    public Transform endPos;
+    public float speed = 2f;
+
+    private bool movingToEnd = true;
+
+    void Update()
+    {
+        MovePlane();
+    }
+
+    void MovePlane()
+    {
+        float step = speed * Time.deltaTime;
+
+        if (movingToEnd)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos.position, step);
+
+            if (transform.position == endPos.position)
+            {
+                movingToEnd = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos.position, step);
+
+            if (transform.position == startPos.position)
+            {
+                movingToEnd = true;
+            }
+        }
+    }
+}
