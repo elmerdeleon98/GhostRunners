0317f8ad4dcfbcc71c032736d5b5ff19889da3cd
Rex <mitchellmoreno7@gmail.com>
2023-12-06 18:37:16 -0800

    Moving Platform Flashlight Stuck Fix
    Platform Fix

--- GhostRunnersProject/Assets/Scripts/MovingPlane.cs
+++ GhostRunnersProject/Assets/Scripts/MovingPlane.cs
@@ -38,14 +38,4 @@ public class MovingPlane : MonoBehaviour
             }
         }
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        other.transform.SetParent(transform);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        other.transform.SetParent(null);
-    }
 }
